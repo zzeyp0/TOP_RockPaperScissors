2dcb5e4e18de8e7792b1873c985f02527dfee4e6
zzeyp0 <125087542+zzeyp0@users.noreply.github.com>
2024-07-04 13:08:45 -0700

    Create UI for rock-paper-scissors game in rps-ui
    DOM manipulation and events ultilized to change the
    game from being played in the console to having a UI
    consisting of choice buttons and div's to display
    the running score and corresponding results text.
    An event listener for the buttons calls playRound()
    which was updated to show each result in the DOM;
    winning text is shown once one player has 5 points.

--- javascript.js
+++ javascript.js
@@ -20,129 +20,79 @@ function getComputerChoice() {
     }
 }
 
-//Step 3: Get human choice
-//via prompt() to return the user inputted string "rock," "paper," or "scissors"
-//given that the input is one of the 3 valid string options, otherwise asks again
-function getHumanChoice() {
-    let flag = 0;
-    let inputString = prompt("To make your play, enter 'rock', 'paper', or 'scissors'");
-    inputString = inputString.toLowerCase();
-
-    while (flag == 0) {
-        if (inputString == "rock" || inputString == "paper" || inputString == "scissors") {
-            flag = 1;
-            return inputString;
-        }
-        else {
-            inputString = prompt("Invalid play, please enter 'rock', 'paper', or 'scissors'");
-        }
-    }
-}
-
 //Step 4: initialize game's scoring variables
 let humanScore = 0;
 let computerScore = 0;
 
 //Step 5: Play a single round given a human choice and computer choice.
 function playRound(humanChoice, computerChoice) {
+    const results = document.querySelector("#results");
+    const runningScore = document.querySelector("#runningScore");
     if (humanChoice == computerChoice) {
-        console.log("It's a draw, no points!")
+        results.textContent = "It's a draw, no points!";
         //humanScore++;
         //computerScore++;
     }
     else if (humanChoice == "rock") {
         if (computerChoice == "scissors") {
-            console.log("You win! Rock beats scissors.");
+            results.textContent = "You win! Rock beats scissors.";
             humanScore++;
         }
         else {
-            console.log("You lose. Paper beats rock.");
+            results.textContent = "You lose. Paper beats rock.";
             computerScore++;
         }
     }
     else if (humanChoice == "paper") {
         if (computerChoice == "rock") {
-            console.log("You win! Paper beats rock.");
+            results.textContent = "You win! Paper beats rock.";
             humanScore++;
         }
         else {
-            console.log("You lose. Scissors beats paper.");
+            results.textContent = "You lose. Scissors beats paper.";
             computerScore++;
         }
     }
     else if (humanChoice == "scissors") {
         if (computerChoice == "paper") {
-            console.log("You win! Scissors beats paper.");
+            results.textContent = "You win! Scissors beats paper.";
             humanScore++;
         }
         else {
-            console.log("You lose. Rock beats scissors.");
+            results.textContent = "You lose. Rock beats scissors.";
             computerScore++;
         }
     }
+
+    runningScore.textContent = "Your score: " + humanScore + "; Computer's score: " + computerScore;
 }
 
-//Step 6: Play an entire game of five rounds with a winner declared at the end
-function playGame() {
-    //Step 4: initialize game's scoring variables
-    let humanScore = 0;
-    let computerScore = 0;
+//announce a winner once 5 pts is reached
+function announceWinner() {
+    if (humanScore == 5 || computerScore == 5) {
+        results.style.backgroundColor = "pink";
 
-    //Step 5: Play a single round given a human choice and computer choice.
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == computerChoice) {
-            console.log("It's a draw!")
-            humanScore++;
-            computerScore++;
+        if (humanScore == 5 && computerScore == 5) {
+            results.textContent = "It's a draw! Your score is: " + humanScore + " and the computer's score is: " + computerScore + ".";
         }
-        else if (humanChoice == "rock") {
-            if (computerChoice == "scissors") {
-                console.log("You win! Rock beats scissors.");
-                humanScore++;
-            }
-            else {
-                console.log("You lose. Paper beats rock.");
-                computerScore++;
-            }
+        else if (humanScore == 5) {
+            results.textContent = "You win! Your score is: " + humanScore + " and the computer's score is: " + computerScore + ".";
         }
-        else if (humanChoice == "paper") {
-            if (computerChoice == "rock") {
-                console.log("You win! Paper beats rock.");
-                humanScore++;
-            }
-            else {
-                console.log("You lose. Scissors beats paper.");
-                computerScore++;
-            }
+        else if (computerScore == 5) {
+            results.textContent = "You lose. Your score is: " + humanScore + " and the computer's score is: " + computerScore + ".";
         }
-        else if (humanChoice == "scissors") {
-            if (computerChoice == "paper") {
-                console.log("You win! Scissors beats paper.");
-                humanScore++;
-            }
-            else {
-                console.log("You lose. Rock beats scissors.");
-                computerScore++;
-            }
-        }
-    }
-    
-    //play five rounds
-    for (i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-
-        playRound(humanChoice, computerChoice);
-    }
-
-    //declare the results
-    if (humanScore == computerScore) {
-        console.log("It's a draw! Your score is: " + humanScore + " and the computer's score is: " + computerScore + ".");
-    }
-    else if (humanScore > computerScore) {
-        console.log("You win! Your score is: " + humanScore + " and the computer's score is: " + computerScore + ".");
-    }
-    else {
-        console.log("You lose. Your score is: " + humanScore + " and the computer's score is: " + computerScore + ".");
-    }
 }
+}
+
+//event listener for all choice buttons
+const buttons = document.querySelectorAll("button");
+    // we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    let computerChoice = getComputerChoice();
+    playRound(button.id, computerChoice);
+    announceWinner();
+  });
+});
+
